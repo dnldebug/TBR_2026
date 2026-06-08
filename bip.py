31ae317fafb190aea1530f86bc9a9a1dcdc05e31
__dnl_ <danillopedro2020@gmail.com>
2026-06-08 11:18:13 -0300

    Add bipar function to control hub speaker

---
+++ bip.py
@@ -0,0 +1,10 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+
+def bipar(frequency=500 , duration=100):
+    hub.speaker.beep(frequency,duration)
