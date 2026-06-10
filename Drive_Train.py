bc379ae543bbdd76ab45d407b0109f32d52cb28d
__dnl_ <danillopedro2020@gmail.com>
2026-06-10 19:02:17 -0300

    Update Drive_Train.py

--- Drive_Train.py
+++ Drive_Train.py
@@ -10,7 +10,7 @@ me = Motor(Port.A, Direction.COUNTERCLOCKWISE)
 md = Motor(Port.B)
 
 
-drive_base = DriveBase(me, md, 54, 115) #a definir.
+drive_base = DriveBase(me, md, 54, 88) #a definir.
 drive_base.use_gyro(True)
 
 
@@ -19,9 +19,15 @@ def andar(dist):
     drive_base.straight(dist*10)
 
 def turn(radius, angle):
+    drive_base.use_gyro(False)
     drive_base.settings(200, 200, 200, 200)
     drive_base.curve(radius, angle)
+    
 
 def rotate(graus):
-    drive_base.settings(220, 220, 220, 220)
+    drive_base.use_gyro(False)
+    drive_base.settings(220, 220, 200, 220)
     drive_base.turn(graus)
+    drive_base.use_gyro(True)
+    #drive_base.heading_control.pid(kp=5,ki=0,kd=12)
+    
